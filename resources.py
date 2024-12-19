f3b6740376204096f747932abf66c6a584d1f67b
ShubhamSNZ <shubhamsnz1125@gmail.com>
2024-12-19 20:09:11 +0530

    resources.py

--- resources.py
+++ resources.py
@@ -1,10 +1,10 @@
+import os
 model_file_path = "food_101//model.keras"
 file_path = "food_101//label.labels.txt"
-# Read the class names into a list
-with open(file_path, 'r') as file:
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the full path to the file
+file_paths = os.path.join(current_dir, file_path)
+# Open the file
+with open(file_paths, 'r') as file:
     class_names = [line.strip() for line in file]
-
-
-if __name__ == '__main__':
-    # Print the class names to verify
-    print(class_names)
