dd89a0a4dac9313901c07506d74b1928ad6b17ee
ShubhamSNZ <shubhamsnz1125@gmail.com>
2024-12-19 18:45:40 +0530

    resources.py

--- resources.py
+++ resources.py
@@ -1,5 +1,5 @@
-model_file_path = "D:\\programming\\Python Programming\\food-101\\model.keras"
-file_path = "D:\\programming\\Python Programming\\food-101\\label.labels.txt"
+model_file_path = "food_101//model.keras"
+file_path = "food_101//label.labels.txt"
 # Read the class names into a list
 with open(file_path, 'r') as file:
     class_names = [line.strip() for line in file]
